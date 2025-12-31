185caccf6429e46e304d18aa7b93bb1ef566c5ca
humaira-jannat <humairajannat926@yahoo.com>
2025-12-31 21:39:19 +0600

    Trigger Netlify deploy

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSYreFXpuFv090iQe2iFq24jAQA1Pwnuk",
-  authDomain: "warmpaws-80ffc.firebaseapp.com",
-  projectId: "warmpaws-80ffc",
-  storageBucket: "warmpaws-80ffc.appspot.com",
-  messagingSenderId: "257549534484",
-  appId: "1:257549534484:web:dff5a6b7a82a261625ee01",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
