c9c419c2d97e1327d9bc18cf7fac94aa7f63386a
humaira-jannat <humairajannat926@yahoo.com>
2025-12-31 20:34:10 +0600

    added privateroute.jsx

---
+++ src/routes/privateRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../providers/AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-orange-500">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
